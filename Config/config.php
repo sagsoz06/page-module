44e46822ced15d9945ff92a27a09304fc1376fce
aniliso <tkaptan41@gmail.com>
2017-04-02 02:52:33 +0300

    Fixed first letter uppercase for pageextension

--- Config/config.php
+++ Config/config.php
@@ -35,7 +35,7 @@ return [
     */
     'relations' => [
         'extension' => function ($self) {
-            return $self->belongsTo(\Modules\PageExtension\Entities\PageExtension::class, 'id', 'page_id')->with(['translations'])->first();
+            return $self->belongsTo(\Modules\Pageextension\Entities\Pageextension::class, 'id', 'page_id')->with(['translations'])->first();
         }
     ],
     /*
