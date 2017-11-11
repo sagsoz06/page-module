083e2980d8e97258d1d7342c31a7e3402710cbc2
aniliso <sagsoz06@gmail.com>
2017-11-11 21:02:27 +0300

    Added show image gallery on settings

--- Resources/views/admin/partials/settings/image.blade.php
+++ Resources/views/admin/partials/settings/image.blade.php
@@ -1,3 +1,12 @@
-@mediaMultiple('pageImage', isset($page) ? $page : null, null, trans('page::pages.form.image'))
-<hr/>
-@mediaSingle('pageCover', isset($page) ? $page : null, null, trans('page::pages.form.cover'))
+<div class="form-group" style="margin-right: 10px;">
+    <label>
+        {!! Form::checkbox("settings[image_gallery]", 1, old('settings.image_gallery', isset($page->settings->image_gallery) ? $page->settings->image_gallery : 0), ['class'=>'flat-blue']) !!}
+        &nbsp; Resimleri Galeri olarak göster
+    </label>
+</div>
+<div class="form-group">
+    @mediaMultiple('pageImage', isset($page) ? $page : null, null, trans('page::pages.form.image'))
+</div>
+<div class="form-group">
+    @mediaSingle('pageCover', isset($page) ? $page : null, null, trans('page::pages.form.cover'))
+</div>
