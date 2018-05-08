49978460c6b1a11908d06e0fe526cdeb3b0fb6ef
aniliso <26839479+aniliso@users.noreply.github.com>
2018-05-08 23:25:54 +0300

    Updating pagewidget

--- Widgets/PageWidget.php
+++ Widgets/PageWidget.php
@@ -18,17 +18,17 @@ class PageWidget
     public function findPage($slug='', $view='find-page')
     {
         if($page = $this->page->findBySlug($slug)) {
-            return view('page::widgets.'.$view, compact('page'));
+            return view('page::widgets.'.$view, compact('page'))->render();
         }
         return false;
     }
 
-    public function children($slug = '', $view='children')
+    public function children($slug = '', $view='children', $limit=10)
     {
         if($page = $this->page->findBySlug($slug))
         {
             if(isset($page->children)) {
-                $children = $page->children()->orderBy('position', 'ASC')->get();
+                $children = $page->children()->orderBy('position', 'ASC')->limit($limit)->get();
                 return view('page::widgets.'.$view, compact('children', 'page'))->render();
             }
         }
