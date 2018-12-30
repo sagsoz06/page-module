e696560e9b3c2d57e4c51ce66cf0f65c72450adb
sagsoz06 <designer@visualturk.net>
2018-12-31 01:58:44 +0300

    Removed override languages

--- Presenters/PagePresenter.php
+++ Presenters/PagePresenter.php
@@ -54,18 +54,6 @@ class PagePresenter extends BasePresenter
         return false;
     }
 
-    public function languages($langKey = 'lang', $urlKey = 'url', $sitemap = false)
-    {
-        $languages = collect();
-        foreach (\LaravelLocalization::getSupportedLocales() as $locale => $supportedLocale)
-        {
-            if($this->entity->hasTranslation($locale)) {
-                $languages->push([$langKey => $locale, $urlKey => $this->entity->is_home ? \LaravelLocalization::getLocalizedURL($locale, route('homepage')) : $this->url($locale)]);
-            }
-        }
-        return $languages->toArray();
-    }
-
     public function files()
     {
         if($files = $this->entity->files()->where('zone', 'pageFiles')->get()) {
